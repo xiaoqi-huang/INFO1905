41aa5e57dff519835069f3becee23e054b5f626c
Xiaoqi Huang <hwangxiaoqi@gmail.com>
2017-11-03 21:38:08 +1100

    Update InPlaceHeapSortTest

--- InPlaceHeapSort/src/InPlaceHeapSortTest.java
+++ InPlaceHeapSort/src/InPlaceHeapSortTest.java
@@ -10,10 +10,8 @@ public class InPlaceHeapSortTest {
 	public void testHeapSort() {
 		int[] actual = new int[]{1, 4, 2, 9, 5, 3, 6};
 		InPlaceHeapSort.heapSort(actual);
-		for (int i = 0; i < actual.length; i++) {
-			System.out.print(actual[i]);
-		}
 		int[] expected = new int[]{9, 6, 5, 4, 3, 2, 1};
+		
 		assertTrue(Arrays.equals(expected, actual));
 	}
 
